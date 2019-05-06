f047321f771188059c4abc817e9a2df35fb1ec6b
Mark Denieffe <madenieffe@gmail.com>
2019-05-06 13:57:55 +0200

    trying to emulate android

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { File } from '@ionic-native/file/ngx';
+import { DataService } from '../data.service';
+
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage  {
 
+  constructor(private file: File) { }	text = 'default text';
+  onChangeText() {
+    this.text = 'New Text!';
+    console.log(this.file.checkDir(this.file.dataDirectory, 'mydir'));
+  }
 }
